50778bd64677a9fea2b61bb854d253d7ed6bda6e
lamouchedu94 <lamouchedu54@gmail.com>
2023-08-09 01:29:09 +0200

    Marche un peu

--- main.go
+++ main.go
@@ -12,26 +12,26 @@ func main() {
 	//var radio_output bytes.Buffer
 	freq := 96000
 
-	command := fmt.Sprintf("rtl_fm -M fm -l 0 -A std -p 0 -s 171k -g 30 -F 9 -f %dK", freq)
-	cmd := exec.Command("bash", "-c", command)
+	command_radio := fmt.Sprintf("rtl_fm -M fm -l 0 -A std -p 0 -s 171k -g 30 -F 9 -f %dK", freq)
+	cmd_radio := exec.Command("bash", "-c", command_radio)
+
+	r, _ := cmd_radio.StdoutPipe()
+
+	var w io.Writer
+	tee := io.TeeReader(r, w)
+
 	wg.Add(1)
-	go flux_radio(cmd)
+	go flux_radio(cmd_radio)
 
 	command_audio := "play -v 0.1 -r 171k -t raw -e s -b 16 -c 1 -V1 - lowpass 16k"
 	cmd_audio := exec.Command("bash", "-c", command_audio)
-
-	//récupère le flux de la première commande
-
-	flux, _ := cmd.StdoutPipe()
-
-	//Utilise le Stdout comme entrée pour la commande d'après
-	cmd_audio.Stdin = flux
+	cmd_audio.Stdin = r
 	wg.Add(2)
-	go audio(flux, cmd_audio)
+	go audio(cmd_audio)
 
 	cmd_RDS := exec.Command("bash", "-c", "redsea --show-partial | jq '.partial_ps'")
-	cmd_RDS.Stdin =
-		wg.Add(3)
+	cmd_RDS.Stdin = tee
+	wg.Add(3)
 	go rds(cmd_RDS)
 
 	for true {
@@ -54,7 +54,7 @@ func rds(cmd_RDS *exec.Cmd) {
 	}
 }
 
-func audio(flux io.ReadCloser, cmd_audio *exec.Cmd) {
+func audio(cmd_audio *exec.Cmd) {
 	err := cmd_audio.Run()
 	if err != nil {
 		fmt.Println(err)
