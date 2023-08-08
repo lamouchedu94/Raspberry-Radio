7d0c6dcc2a88811a09e23a806a41e204ff5d66d7
lamouchedu94 <lamouchedu54@gmail.com>
2023-08-09 01:00:04 +0200

    passage en go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module rasp-radio
+
+go 1.21.0
--- testRDS.py
+++ testRDS.py
@@ -14,9 +14,11 @@ import subprocess
 import threading
 import time
 
-myPopen = subprocess.Popen("rtl_fm -M fm -l 0 -A std -p 0 -s 171k -g 30 -F 9 -f 96M" , shell = True, stdout = subprocess.PIPE)
+myPopen = subprocess.Popen("rtl_fm -M fm -l 0 -A std -p 0 -s 171k -g 30 -F 9 -f 90.4M" , shell = True, stdout = subprocess.PIPE)
 cmd2 = subprocess.Popen("play -v 0.03 -r 171k -t raw -e s -b 16 -c 1 -V1 - lowpass 16k", stdin=myPopen.stdout , shell = True)
-cmd3 = subprocess.Popen("redsea --show-partial | jq '.partial_ps'",stdin=myPopen.stdout , shell = True)
-
-
+#cmd3 = subprocess.Popen("redsea --show-partial | jq '.partial_ps'",stdin=myPopen.stdout , shell = True,)
+i = 0 
+while True :
+    i += 1
+    print(i)
 
